69b11450a05b4c2e285dbad09e2cea1f194dc72a
Noonlord <mymmym22@gmail.com>
2023-05-02 05:11:25 +0300

    Fixup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), nodePolyfills({ protocolImports: true })],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
