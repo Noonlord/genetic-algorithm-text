75185683e43abb0ed0a9e7c9a70de6193025b73a
Noonlord <mymmym22@gmail.com>
2023-05-03 08:26:00 +0300

    Modify readme

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,6 @@ import { RouterLink } from 'vue-router';
 <template>
   <header class="flex px-12 justify-between bg-blue-100 rounded-b-md">
     <img alt="Vue logo" class="w-16 h-16" src="@/assets/logo.svg" />
-    <div class="flex gap-2 w-fit my-auto">
-      <RouterLink to="/" class="my-auto">Home</RouterLink>
-      <RouterLink to="/about" class="my-auto">About</RouterLink>
-    </div>
+    <div class="flex gap-2 w-fit my-auto"></div>
   </header>
 </template>
